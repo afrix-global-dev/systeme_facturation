29247e78af2dd7a1596233ed92050564ec3be6b8
laurence1Mas <laurencemasika01@gmail.com>
2026-04-18 11:58:29 +0200

    feat: connect client page (frontend) with the Api

--- apps/frontend/src/store/store.ts
+++ apps/frontend/src/store/store.ts
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 // import authReducer from './slices/authSlice';
 import authReducer from './slices/authSlice';
+import clientReducer from './slices/clientSlice';
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    clients: clientReducer,
   },
 });
 
